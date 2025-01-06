c42d95b59a3bbb9630e0d721068a60f97b47f1b5
Fernando Braga Alves <fbragaalves97@gmail.com>
2025-01-05 22:23:10 -0300

    feat: add actio para ver PDF

--- app/Filament/Resources/InvoiceResource.php
+++ app/Filament/Resources/InvoiceResource.php
@@ -56,6 +56,11 @@ class InvoiceResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('seePdf')
+                    ->label('Ver PDF')
+                    ->icon('heroicon-o-document')
+                    ->disabled(fn ($record) => ! $record->filepath)
+                    ->url(fn ($record) => route('invoices.pdf', $record), true),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
